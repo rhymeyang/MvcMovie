ab141cda05a2f194285c0463bc984efabdaaf959
yayavicky <yaya_vicky@hotmail.com>
2024-05-16 15:04:04 -0400

    string parameter for Welcome

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -7,14 +7,14 @@ public class HelloWorldController : Controller
 {
     // 
     // GET: /HelloWorld/
-    public string Index()
+    public IActionResult Index()
     {
-        return "This is my default action...";
+        return View();
     }
     // 
     // GET: /HelloWorld/Welcome/ 
-    public string Welcome()
+    public string Welcome(string name, int numTimes = 1)
     {
-        return "This is the Welcome action method...";
+        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
     }
 }
